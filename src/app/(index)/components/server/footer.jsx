4a79cbb6b4084df416f9411253b4c1c650cee999
Rahul Das <77882624+rahulcodepython@users.noreply.github.com>
2023-10-01 01:09:23 +0530

    Dashboard and Footer is added.

--- src/app/(index)/components/server/footer.jsx
+++ src/app/(index)/components/server/footer.jsx
@@ -1,23 +1,20 @@
-// import React from 'react'
-// import { AiOutlineCopyright } from '@/data/icons'
+import React from 'react'
+import { AiOutlineCopyright } from '@/data/icons/icons'
 
 const Footer = ({ showSidebar }) => {
     return (
-        <div>
-            Footer
-        </div>
-        // <footer className={`fixed z-10 bottom-0 py-3 bg-primary w-full ${showSidebar ? 'lg:pl-64 duration-500' : 'pl-0 duration-500'} flex items-center justify-between`}>
-        //     <div className='flex items-center text-white gap-2 font-semibold pl-3 sm:pl-5 lg:pl-10'>
-        //         <AiOutlineCopyright />
-        //         2023, Made by Rahul Das
-        //     </div>
-        //     <div className='flex items-center gap-4 text-purple-700 font-semibold pr-3 sm:pr-5 lg:pr-10'>
-        //         <span>License</span>
-        //         <span>More Themes</span>
-        //         <span>Documentation</span>
-        //         <span>Support</span>
-        //     </div>
-        // </footer>
+        <footer className="py-3 bg-black w-full flex items-center justify-between">
+            <div className='flex items-center text-white gap-2 font-semibold pl-3 sm:pl-5 lg:pl-10'>
+                <AiOutlineCopyright />
+                2023, Made by Rahul Das
+            </div>
+            <div className='flex items-center gap-4 text-white font-semibold pr-3 sm:pr-5 lg:pr-10'>
+                <span className='hover:text-slate-500 cursor-pointer'>License</span>
+                <span className='hover:text-slate-500 cursor-pointer'>More Themes</span>
+                <span className='hover:text-slate-500 cursor-pointer'>Documentation</span>
+                <span className='hover:text-slate-500 cursor-pointer'>Support</span>
+            </div>
+        </footer>
     )
 }
 
--- src/app/(index)/layout.jsx
+++ src/app/(index)/layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavbarSidebarLayout from './components/client/NavbarSidebarLayout '
+import Footer from './components/server/Footer'
 
 const DashboardLayout = ({ children }) => {
     return (
         <>
             <NavbarSidebarLayout />
             {children}
+            <Footer />
         </>
     )
 }
